9894e04991e6a1983e5c934552bd7b1e99ccf9a2
tiancaiV <wifiway@163.com>
2025-04-15 21:04:52 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -37,6 +37,8 @@ echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 echo "CONFIG_USE_APK=n" >> ./.config
+echo "CONFIG_PACKAGE_opkg=y" ./.config
+
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
 	echo -e "$WRT_PACKAGE" >> ./.config
